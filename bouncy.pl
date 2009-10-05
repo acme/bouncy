444b7a8c4eed6e7361b44e879f4abf38deb71d2d
Leon Brocard <acme@astray.com>
2009-10-05 21:16:21 +0100

    make it bounce

--- bouncy.pl
+++ bouncy.pl
@@ -26,13 +26,13 @@ my $app_rect = SDL::Rect->new(
 
 my $background = $SDL::Color::black;
 
-my $sprite = SDL::Surface->new( -name => "ball.png" );
-$sprite->display_format();
-my $sprite_rect = SDL::Rect->new(
+my $ball = SDL::Surface->new( -name => "ball.png" );
+$ball->display_format();
+my $ball_rect = SDL::Rect->new(
     -x      => 0,
     -y      => 0,
-    -width  => $sprite->width,
-    -height => $sprite->height,
+    -width  => $ball->width,
+    -height => $ball->height,
 );
 my $event = SDL::Event->new();
 
@@ -40,15 +40,17 @@ sub put_sprite {
     my ( $x, $y ) = @_;
 
     my $dest_rect = SDL::Rect->new(
-        -x => $x - ( $sprite->width / 2 ),
-        -y => $y - ( $sprite->height / 2 ),
-        -width  => $sprite->width,
-        -height => $sprite->height,
+        -x => $x - ( $ball->width / 2 ),
+        -y => $y - ( $ball->height / 2 ),
+        -width  => $ball->width,
+        -height => $ball->height,
     );
-    $sprite->blit( $sprite_rect, $app, $dest_rect );
+    $ball->blit( $ball_rect, $app, $dest_rect );
 }
 
-my $i = 0;
+my ( $x, $y ) = ( $screen_width / 2, $screen_height / 2 );
+my $dx = 1;
+my $dy = 0;
 while (1) {
 
     # process event queue
@@ -61,20 +63,40 @@ while (1) {
     last if ( SDL::GetKeyState(SDLK_ESCAPE) );
     last if ( $etype eq SDL_KEYDOWN );
 
-    $app->fill( $app_rect, $background );
-
-    my $delta = 40;
-    my $x     = 0;
-    foreach ( 1 .. 16 ) {
-        put_sprite(
-            $x + sin( $i + $_ / 2 ) * $_,
-            $screen_height / 2
-                + ( sin( ( $i + $_ ) * 0.2 ) * ( $screen_height / 3 ) )
-        );
-        $x += $delta;
+    if ( $etype eq SDL_MOUSEBUTTONDOWN ) {
+        $x  = $event->button_x;
+        $y  = $event->button_y;
+        $dx = 1;
+        $dy = -1;
     }
-    $i += 0.05;
 
+    $app->fill( $app_rect, $background );
+    put_sprite( $x, $y );
+
+    $x += $dx;
+    if ( $x + $ball->width / 2 > $screen_width ) {
+        $dx = $dx * -0.9;
+        $dy = $dy * 0.9;
+        $x += $dx;
+    }
+    if ( $x - $ball->width / 2 < 0 ) {
+        $dx = $dx * -0.9;
+        $dy = $dy * 0.9;
+        $x += $dx;
+    }
+
+    $y  += $dy;
+    $dy += 0.002;
+    if ( $y + $ball->height / 2 > $screen_height ) {
+        $dy = $dy * -0.9;
+        $dx = $dx * 0.9;
+        $y += $dy;
+    }
+    if ( $y - $ball->height / 2 < 0 ) {
+        $dy = $dy * -1.1;
+        $dx = $dx * 0.9;
+        $y += $dy;
+    }
     $app->sync;
 }
 
