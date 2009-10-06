7ee5887a5b3e3d82c0c7949ff3b440f4ecdf92e2
Leon Brocard <acme@astray.com>
2009-10-06 11:01:34 +0100

    and more like batty

--- bouncy.pl
+++ bouncy.pl
@@ -14,8 +14,7 @@ my $screen_height = 480;
 my $app = SDL::App->new(
     -width  => 640,
     -height => 480,
-    -icon   => 'ball.png',
-    -flags  => SDL_ANYFORMAT | SDL_HWACCEL
+    -flags => SDL_ANYFORMAT | SDL_HWACCEL
         | SDL_RLEACCEL,    # SDL_HWACCEL SDL_DOUBLEBUF SDL_ANYFORMAT
 );
 
@@ -24,9 +23,9 @@ my $app_rect = SDL::Rect->new(
     -width  => $screen_width,
 );
 
-my $background = $SDL::Color::black;
+my $background = $SDL::Color::yellow;
 
-my $ball = SDL::Surface->new( -name => "ball.png" );
+my $ball = SDL::Surface->new( -name => 'ball2.png' );
 $ball->display_format();
 my $ball_rect = SDL::Rect->new(
     -x      => 0,
@@ -34,23 +33,36 @@ my $ball_rect = SDL::Rect->new(
     -width  => $ball->width,
     -height => $ball->height,
 );
+
+my $bat = SDL::Surface->new( -name => 'bat.png' );
+$bat->display_format();
+my $bat_rect = SDL::Rect->new(
+    -x      => 0,
+    -y      => 0,
+    -width  => $bat->width,
+    -height => $bat->height,
+);
+
 my $event = SDL::Event->new();
 
 sub put_sprite {
-    my ( $x, $y ) = @_;
+    my ( $x, $y, $source, $source_rect ) = @_;
 
     my $dest_rect = SDL::Rect->new(
-        -x => $x - ( $ball->width / 2 ),
-        -y => $y - ( $ball->height / 2 ),
-        -width  => $ball->width,
-        -height => $ball->height,
+        -x      => $x,
+        -y      => $y,
+        -width  => $source->width,
+        -height => $source->height,
     );
-    $ball->blit( $ball_rect, $app, $dest_rect );
+    $source->blit( $source_rect, $app, $dest_rect );
 }
 
-my ( $x, $y ) = ( $screen_width / 2, $screen_height / 2 );
-my $dx = 1;
-my $dy = 0;
+my $bat_x = 100;
+my $bat_y = $screen_height - $bat->height;
+my ( $x, $y ) = ( $bat_x + 54, $bat_y );
+
+my $dx = 0.2;
+my $dy = -1.25;
 while (1) {
 
     # process event queue
@@ -64,22 +76,28 @@ while (1) {
     last if ( $etype eq SDL_KEYDOWN );
 
     if ( $etype eq SDL_MOUSEBUTTONDOWN ) {
-        $x  = $event->button_x;
-        $y  = $event->button_y;
+        $x  = $bat_x + $bat->width / 3;
+        $y  = $bat_y;
         $dx = 1;
-        $dy = -1;
+        $dy = -1.3;
+    }
+    if ( $etype eq SDL_MOUSEMOTION ) {
+        $bat_x = $event->motion_x - 56;
+        $bat_x = 0 if $bat_x < 0;
+        $bat_x = $screen_width - 112 if $bat_x + 112 > $screen_width;
     }
 
     $app->fill( $app_rect, $background );
-    put_sprite( $x, $y );
+    put_sprite( $x,     $y - $ball->height, $ball, $ball_rect );
+    put_sprite( $bat_x, $bat_y,             $bat,  $bat_rect );
 
     $x += $dx;
-    if ( $x + $ball->width / 2 > $screen_width ) {
+    if ( $x + $ball->width > $screen_width ) {
         $dx = $dx * -0.9;
         $dy = $dy * 0.9;
         $x += $dx;
     }
-    if ( $x - $ball->width / 2 < 0 ) {
+    if ( $x < 0 ) {
         $dx = $dx * -0.9;
         $dy = $dy * 0.9;
         $x += $dx;
@@ -87,12 +105,18 @@ while (1) {
 
     $y  += $dy;
     $dy += 0.002;
-    if ( $y + $ball->height / 2 > $screen_height ) {
-        $dy = $dy * -0.9;
-        $dx = $dx * 0.9;
+    if ( ( $x + $ball->width / 2 > $bat_x && $x < $bat_x + 108 )
+        && $y > $screen_height - $bat->height + 5 )
+    {
+        $dy = -1.25;
+        $dx = 0.5 * $dx + ( $x + $ball->width / 2 - $bat_x - 56 ) / 100;
+        $y += $dy;
+    } elsif ( $y > $screen_height ) {
+        $dy = $dy * -0.7;
+        $dx = $dx * 0.7;
         $y += $dy;
     }
-    if ( $y - $ball->height / 2 < 0 ) {
+    if ( $y - $ball->height < 0 ) {
         $dy = $dy * -1.1;
         $dx = $dx * 0.9;
         $y += $dy;
